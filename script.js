14cb3f460d42a3585bd1496be55899351531d9f3
samkayw <83036429+samkayw@users.noreply.github.com>
2025-06-30 16:51:33 -0700

    JS Practice 2

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ x = 5;
 y = 6;
 z = x + y;
 
-console.log(z)
+const r = 4, q = 2;
+
+console.log(r + 2)
+
+console.log(z + x)
+
+
+let canVote = 18;
+let message = canVote >= 50 ? "No" : "Yes" 
+console.log(message)
+
+
+let loggedIn = true;
+let buttonText = loggedIn ? "Logout" : "Login";
+
+console.log(buttonText); 
