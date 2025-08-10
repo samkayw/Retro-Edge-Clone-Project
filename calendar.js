a6064a2d95ed39e9e7ad30daa4da2e0b3303a07c
samkayw <83036429+samkayw@users.noreply.github.com>
2025-08-10 11:11:11 -0700

    Removing Sign In

--- calendar.js
+++ calendar.js
@@ -7,22 +7,23 @@
       /* exported handleSignoutClick */
 
       // TODO(developer): Set to client ID and API key from the Developer Console
-      const CLIENT_ID = '300093958729-8g1fr9o7catq7jtgh26gd6293d0n6qfh.apps.googleusercontent.com';
+    //   const CLIENT_ID = '300093958729-8g1fr9o7catq7jtgh26gd6293d0n6qfh.apps.googleusercontent.com';
       const API_KEY = 'AIzaSyACeAr4hZMJ9axLPEMzLPZgQr8XkYAZl1k';
+      const PUBLIC_CAL_ID = '3cae1ba52ac23bca99893a1b81869bf4fd06aae62a79f167b7ac76510d26c045@group.calendar.google.com';
 
       // Discovery doc URL for APIs used by the quickstart
       const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
       // Authorization scopes required by the API; multiple scopes can be
       // included, separated by spaces.
-      const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+    //   const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-      let tokenClient;
-      let gapiInited = false;
-      let gisInited = false;
+    //   let tokenClient;
+    //   let gapiInited = false;
+    //   let gisInited = false;
 
-      document.getElementById('authorize_button').style.visibility = 'hidden';
-      document.getElementById('signout_button').style.visibility = 'hidden';
+    //   document.getElementById('authorize_button').style.visibility = 'hidden';
+    //   document.getElementById('signout_button').style.visibility = 'hidden';
 
       /**
        * Callback after api.js is loaded.
@@ -40,70 +41,71 @@
           apiKey: API_KEY,
           discoveryDocs: [DISCOVERY_DOC],
         });
-        gapiInited = true;
-        maybeEnableButtons();
+        // gapiInited = true;
+          // maybeEnableButtons();
+          listUpcomingEvents();
       }
 
       /**
        * Callback after Google Identity Services are loaded.
        */
-      function gisLoaded() {
-        tokenClient = google.accounts.oauth2.initTokenClient({
-          client_id: CLIENT_ID,
-          scope: SCOPES,
-          callback: '', // defined later
-        });
-        gisInited = true;
-        maybeEnableButtons();
-      }
+    //   function gisLoaded() {
+    //     tokenClient = google.accounts.oauth2.initTokenClient({
+    //       client_id: CLIENT_ID,
+    //       scope: SCOPES,
+    //       callback: '', // defined later
+    //     });
+    //     // gisInited = true;
+    //     // maybeEnableButtons();
+    //   }
 
       /**
        * Enables user interaction after all libraries are loaded.
        */
-      function maybeEnableButtons() {
-        if (gapiInited && gisInited) {
-          document.getElementById('authorize_button').style.visibility = 'visible';
-        }
-      }
+    //   function maybeEnableButtons() {
+    //     if (gapiInited && gisInited) {
+    //       document.getElementById('authorize_button').style.visibility = 'visible';
+    //     }
+    //   }
 
       /**
        *  Sign in the user upon button click.
        */
 // SIGN IN 
-      function handleAuthClick() {
-        tokenClient.callback = async (resp) => {
-          if (resp.error !== undefined) {
-            throw (resp);
-          }
-          document.getElementById('signout_button').style.visibility = 'visible';
-          document.getElementById('authorize_button').innerText = 'Refresh';
-          await listUpcomingEvents();
-        };
+    //   function handleAuthClick() {
+    //     tokenClient.callback = async (resp) => {
+    //       if (resp.error !== undefined) {
+    //         throw (resp);
+    //       }
+    //       document.getElementById('signout_button').style.visibility = 'visible';
+    //       document.getElementById('authorize_button').innerText = 'Refresh';
+    //       await listUpcomingEvents();
+    //     };
 
-        if (gapi.client.getToken() === null) {
-          // Prompt the user to select a Google Account and ask for consent to share their data
-          // when establishing a new session.
-          tokenClient.requestAccessToken({prompt: 'consent'});
-        } else {
-          // Skip display of account chooser and consent dialog for an existing session.
-          tokenClient.requestAccessToken({prompt: ''});
-        }
-      }
+    //     if (gapi.client.getToken() === null) {
+    //       // Prompt the user to select a Google Account and ask for consent to share their data
+    //       // when establishing a new session.
+    //       tokenClient.requestAccessToken({prompt: 'consent'});
+    //     } else {
+    //       // Skip display of account chooser and consent dialog for an existing session.
+    //       tokenClient.requestAccessToken({prompt: ''});
+    //     }
+    //   }
 // SIGN IN
       /**
        *  Sign out the user upon button click.
        */
 //SIGN OUT
-      function handleSignoutClick() {
-        const token = gapi.client.getToken();
-        if (token !== null) {
-          google.accounts.oauth2.revoke(token.access_token);
-          gapi.client.setToken('');
-          document.getElementById('content').innerText = '';
-          document.getElementById('authorize_button').innerText = 'Authorize';
-          document.getElementById('signout_button').style.visibility = 'hidden';
-        }
-      }
+    //   function handleSignoutClick() {
+    //     const token = gapi.client.getToken();
+    //     if (token !== null) {
+    //       google.accounts.oauth2.revoke(token.access_token);
+    //       gapi.client.setToken('');
+    //       document.getElementById('content').innerText = '';
+    //       document.getElementById('authorize_button').innerText = 'Authorize';
+    //       document.getElementById('signout_button').style.visibility = 'hidden';
+    //     }
+    //   }
 //SIGN OUT
       /**
        * Print the summary and start datetime/date of the next ten events in
@@ -119,7 +121,7 @@
         try {
 // API Calls List
           const request = {
-            'calendarId': '3cae1ba52ac23bca99893a1b81869bf4fd06aae62a79f167b7ac76510d26c045@group.calendar.google.com',
+            'calendarId': PUBLIC_CAL_ID,
             'timeMin': (new Date()).toISOString(),
             'showDeleted': false,
             'singleEvents': true,
@@ -316,6 +318,7 @@ function renderCalendar(events, month, year) {
     cGrid.addEventListener('click', expandToolTip)
 
 
+
     //toggle the classes 
     function expandToolTip(e) {
         // grabs the closest calendar icon
@@ -325,28 +328,38 @@ function renderCalendar(events, month, year) {
         const card = icon.closest('.event');          //find the icon's event ifo
         const tip = card.querySelector('.tooltiptext'); //get the event info's tooltip
         if (!tip) return;
+        // const isOpen = true
 
-        const eventMenu = cGrid.querySelectorAll('.tooltiptext.expand') //gather up any open tool tip
-        eventMenu.forEach(t => t.classList.remove('expand')); //close them before you open the next
+
+        //checks if other tooltips are open other than the one we have open right now
+        const openTips = cGrid.querySelectorAll('.tooltiptext.expand');
+        openTips.forEach(t => {
+            if (t !== tip) t.classList.remove('expand');
+        });
+
+        // const eventMenu = cGrid.querySelectorAll('.tooltiptext.expand') //gather up any open tool tip
+        // eventMenu.forEach(t => t.classList.toggle('expand')); //close them before you open the next
+
+
         tip.classList.toggle('expand'); //toggle it
 
 
     }
 
-    
+    //
     document.addEventListener('click', function (e) {
-        
         if (e.target.className !== 'calendar-icon') {
             // closes it if anything other than an icon is clicked
             const eventMenu = cGrid.querySelectorAll('.tooltiptext.expand')
             eventMenu.forEach(t => t.classList.remove('expand'));
             
-            
-
-        } 
+        }
     
-    }
-)}
+    })
+
+        
+      
+}
 
 
 
